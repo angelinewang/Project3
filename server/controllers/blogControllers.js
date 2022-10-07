45963828ed00e023083f2a22a2644e32d427e4dc
Amal <amal.mukhtar@outlook.com>
2022-10-07 17:15:48 +0100

    Added routes and controllers

--- server/controllers/blogControllers.js
+++ server/controllers/blogControllers.js
@@ -1,11 +1,60 @@
 import Blog from "../models/Blog";
+import { CastError } from "mongoose";
 
 export async function getAllBlogs(req, res, next) {
   try {
     const blogs = await Blog.find();
     return res.json(blogs);
-    res.status;
   } catch (err) {
     next(err);
+  }
+}
+
+export async function getABlog(req, res, next) {
+  try {
+    const blog = await Blog.findById(req.params.id);
+    if (!blog) {
+      return res.status(400).json({ error: true, message: "Blog not found." });
+    }
+  } catch (error) {
+    if (error instanceof CastError) {
+      res
+        .status(400)
+        .send({ error: "Invalid id - please enter the correct id." });
+    } else {
+      next(error);
+    }
+  }
+}
+
+export async function createBlog(req, res, next) {
+  try {
+    const blog = await Blog(req.body);
+    res.json(blog);
+  } catch (error) {
+    res.status(400).send({
+      error:
+        "You must provide a title, description and content when creating a blog.",
+    });
+  }
+}
+
+export async function updateBlog(req, res, next) {
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json(updateBlog);
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function deleteBlog(req, res, next) {
+  try {
+    await Blog.findByIdAndDelete(req.params.id);
+    res.status(204).send();
+  } catch (error) {
+    next(error);
   }
 }
--- server/routes/api/blogs.js
+++ server/routes/api/blogs.js
@@ -1,7 +1,17 @@
 import * as blogCtrls from "../../controllers/blogControllers";
 import { Router } from "express";
+import { checkAuth } from "../../middlewares/auth.js";
 
 const router = Router();
-router.route("/").get(blogCtrls.getAllBlogs);
+router
+  .route("/")
+  .get(blogCtrls.getAllBlogs)
+  .post(checkAuth, blogCtrls.createBlog);
+
+router
+  .route("/:id")
+  .get(blogCtrls.getABlog)
+  .patch(checkAuth, blogCtrls.updatedBlog)
+  .delete(checkAuth, blogCtrls.deleteBlog);
 
 export default router;
