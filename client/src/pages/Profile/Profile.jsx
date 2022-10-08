e1d4a94f81b4bc50e24605230501d898b64b7f52
Amal <amal.mukhtar@outlook.com>
2022-10-08 17:56:17 +0100

    Added to profile, created a user blogs comp,  and updated routes

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import useUser from "../../hooks/useUser";
 import { getUserBlog } from "../../utils/blogService";
+import HTMLReactParser from "html-react-parser";
 
 function Profile() {
   const { user } = useUser();
@@ -33,27 +34,35 @@ function Profile() {
 
   // TODO: Display most recent blogs by user
   // TODO: Display link to user's blog posts
+  // TODO: Upload / edit profile picture
+  // TODO: Upload / edit bio
 
   return (
     <div>
       <section>
-        {user ? <p> User: {user.name}</p> : null}
-        <p>Joined: {blog.createdAt}</p>
-        <p>Blogs: {blog.blogs.length}</p>
+        {user ? <h2> {user.name}</h2> : null}
+
+        {blog ? (
+          <div>
+            <p>User: {blog.name}</p>
+            <p>Joined: {blog.createdAt}</p>
+            <p>Blogs: {blog.blogs.length}</p>
+            <h2> Blogs by {blog.name}</h2>
+            {blog.blogs.map((b) => (
+              <>
+                <Link to={`/blog/${b._id}`}>
+                  <h3>{b.title}</h3>
+                </Link>
+
+                <div>{HTMLReactParser(b.content)}</div>
+              </>
+            ))}
+            <Link to={`/profile/${blog._id}/blogs`}>
+              <p>View more of {blog.name}'s blogs </p>
+            </Link>
+          </div>
+        ) : null}
       </section>
-      <section>
-        <h2> Blogs by {blog.name}</h2>
-        {blog
-          ? blog.blogs.map((b) => (
-              <ul>
-                <li>{b.title}</li>
-              </ul>
-            ))
-          : null}
-      </section>
-      <div>
-        <p>View more of {blog.name}'s blogs -> </p>
-      </div>
     </div>
   );
 }
