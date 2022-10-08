9edc851a28cbfb1ac6f350b04daf5340131460c2
ANGELINE WANG <02ANGELINEWANG@GMAIL.COM>
2022-10-08 12:02:26 +0100

    Server errors

--- server/routes/api/blogs.js
+++ server/routes/api/blogs.js
@@ -1,17 +1,17 @@
-import * as blogCtrls from "../../controllers/blogControllers";
+import * as blogCtrl from "../../controllers/blogControllers.js";
 import { Router } from "express";
 import { checkAuth } from "../../middlewares/auth.js";
 
 const router = Router();
 router
   .route("/")
-  .get(blogCtrls.getAllBlogs)
-  .post(checkAuth, blogCtrls.createBlog);
+  .get(blogCtrl.getAllBlogs)
+  .post(checkAuth, blogCtrl.createBlog);
 
 router
   .route("/:id")
-  .get(blogCtrls.getABlog)
-  .patch(checkAuth, blogCtrls.updatedBlog)
-  .delete(checkAuth, blogCtrls.deleteBlog);
+  .get(blogCtrl.getABlog)
+  .patch(checkAuth, blogCtrl.updateBlog)
+  .delete(checkAuth, blogCtrl.deleteBlog);
 
 export default router;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 
@@ -7,7 +8,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import userRoutes from "./routes/api/users.js";
-import blogRoutes from "./routes/api/blogs";
+import blogRoutes from "./routes/api/blogs.js";
 import auth from "./config/auth.js";
 
 import { connectToDb } from "./config/database.js";
