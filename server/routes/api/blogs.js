68da219a15fd09c85e3490951c5ef5c6188fdc68
Manohisoa <manohisoarajaonarivony@gmail.com>
2022-10-12 16:18:02 +0300

    created the upload middleware and added it to blog route

--- server/routes/api/blogs.js
+++ server/routes/api/blogs.js
@@ -1,12 +1,13 @@
 import blogControllers from "../../controllers/blogControllers.js";
 import { Router } from "express";
 import { checkAuth } from "../../middlewares/auth.js";
+import uploadFile from "../../middlewares/upload.js";
 
 const router = Router();
 router
   .route("/")
   .get(blogControllers.getAllBlogs)
-  .post(checkAuth, blogControllers.createBlog);
+  .post(checkAuth, uploadFile, blogControllers.createBlog);
 
 router
   .route("/:id")
