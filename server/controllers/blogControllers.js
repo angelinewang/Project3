c822026ff64ee7f31fa179e270a503ab92b9e9a5
ANGELINE WANG <02ANGELINEWANG@GMAIL.COM>
2022-10-17 10:37:48 +0100

    Added Image reference to Blog Schema so that it can be found and deleted upon Blog deletion

--- server/controllers/blogControllers.js
+++ server/controllers/blogControllers.js
@@ -1,5 +1,6 @@
 import Blog from "../models/blog.js";
 import User from "../models/user.js";
+import Image from "../models/image.js";
 import { CastError } from "mongoose";
 
 async function getAllBlogs(req, res, next) {
@@ -58,11 +59,17 @@ async function createBlog(req, res, next) {
     let currentUser = await User.findById(userId);
     const data = req.body;
     data.author = userId;
-    data.image = filePath;
+
+    //The image field within the Blog must be the ObjectId of the Image in order to delete the image on Blog deletion
+    const newImage = await Image.create(filePath);
+    data.image = newImage._id;
+
     const newBlog = await Blog.create(data);
     console.log(newBlog);
+
     currentUser.blogs.push(newBlog._id);
     await currentUser.save();
+
     res.json(newBlog);
   } catch (error) {
     res.status(400).json(error);
@@ -82,8 +89,16 @@ async function updatedBlog(req, res, next) {
 
 async function deleteBlog(req, res, next) {
   try {
+    let oldBlog = await Blog.findById(req.params.id).populate("image");
+
+    await Image.findByIdAndDelete(oldBlog.image._id);
+
     await Blog.findByIdAndDelete(req.params.id);
     res.status(204).send();
+
+    //There is no way to find and delete the Image from the uploads folder without a reference to its ObjectID with the Blog Schema
+    //Thus, I have changed the data saved under Image inside the Blog into the reference to the Image instead
+    //You can ignore these changes if you wish to keep the origin schema, however that just means we cannot delete the image upon Blog deletion
   } catch (error) {
     next(error);
   }
--- server/models/blog.js
+++ server/models/blog.js
@@ -4,17 +4,18 @@ const commentSchema = new mongoose.Schema({
   comments: [String],
 });
 
-const tagSchema = new mongoose.Schema({
-  tags: [String],
-});
+//Deleted tag schema since it was not in use
 
 const blogSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
     description: { type: String, required: true },
     content: { type: String, required: true },
-    // image: { data: Buffer, contentType: String },
-    image: String,
+    image: { type: mongoose.Schema.ObjectId, ref: "Image" },
+    //There is no way to find and delete the Image from the uploads folder without a reference to its ObjectID with the Blog Schema
+    //Thus, I have changed the data saved under Image inside the Blog into the reference to the Image instead
+    //You can ignore these changes if you wish to keep the origin schema, however that just means we cannot delete the image upon Blog deletion
+    // image: String,
     tags: [String],
     comments: [commentSchema],
     //18 and 19 need double checking
