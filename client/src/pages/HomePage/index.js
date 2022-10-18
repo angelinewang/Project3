3a5e8fe1ea37c8f66cde3fc3c6e5c3be9a31176d
ANGELINE WANG <02ANGELINEWANG@GMAIL.COM>
2022-10-18 16:55:52 +0100

    Homepage Base URL

--- client/src/pages/HomePage/index.js
+++ client/src/pages/HomePage/index.js
@@ -16,7 +16,9 @@ export default function HomePage() {
 
   async function getBlogs() {
     try {
-      const response = await fetch("/api/blogs");
+      const response = await fetch(
+        "https://blogging-platform-365219.ew.r.appspot.com/api/blogs"
+      );
       const blogs = await response.json();
       setBlogs(blogs);
     } catch (err) {
@@ -26,7 +28,9 @@ export default function HomePage() {
 
   async function getTags() {
     try {
-      const response = await fetch("/api/blogs");
+      const response = await fetch(
+        "https://blogging-platform-365219.ew.r.appspot.com/api/blogs"
+      );
       const blogs = await response.json();
       blogs.forEach((blog) => {
         blog.tags.forEach((tag) => {
