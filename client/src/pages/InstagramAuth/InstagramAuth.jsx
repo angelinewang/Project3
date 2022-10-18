c01ab55286ff217107cbdcd444b371e9345ada1a
ANGELINE WANG <02ANGELINEWANG@GMAIL.COM>
2022-10-18 11:02:26 +0100

    Changing IG route

--- client/src/pages/InstagramAuth/InstagramAuth.jsx
+++ client/src/pages/InstagramAuth/InstagramAuth.jsx
@@ -3,7 +3,7 @@ import './InstagramAuth.css';
 function InstagramAuth() {
 
     // /instagram/auth automatically redirects to page for Instagram Authentication
-window.location.replace("https://api.instagram.com/oauth/authorize?client_id=6002320329798591&redirect_uri=https://celebrated-salmiakki-a8925d.netlify.app/instagram/auth&scope=user_profile,user_media&response_type=code")
+window.location.replace("https://api.instagram.com/oauth/authorize?client_id=6002320329798591&redirect_uri=https://celebrated-salmiakki-a8925d.netlify.app/instagram/photos&scope=user_profile,user_media&response_type=code")
 
 };
 
