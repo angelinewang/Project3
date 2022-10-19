273e0704068b95c35fa7ee6eca9df6f8399923e1
ANGELINE WANG <02ANGELINEWANG@GMAIL.COM>
2022-10-19 14:12:00 +0100

    Testing Change for Cloud Run

--- client/src/utils/blogService.js
+++ client/src/utils/blogService.js
@@ -13,6 +13,7 @@ export const getBlogs = async () => {
 export const getBlog = async (blogID) => {
   try {
     let res = await fetch(BASE_URL + `/api/blogs/${blogID}`);
+
     return res.json();
   } catch (error) {
     console.log(error);
@@ -21,8 +22,18 @@ export const getBlog = async (blogID) => {
 
 export const getUserBlog = async (userID) => {
   try {
+    //1. Does the user ID exist? YES 2. Does the URL work for blogs? YES 3. Does the URL work for other users calls?
+
+    //4. Try users/login API WORKS 5. Try users/signup APi WORKS
     // console.log("service ran");
     let res = await fetch(BASE_URL + `/api/users/${userID}`);
+    // #1 Test this API request in Postman: https://blogging-platform-365219.ew.r.appspot.com/api/users/6341888e3ef0c4526bc0d222
+
+    // #1.5 See if correct user id is being passed into the API
+
+    // #2 Finish making Profile work
+    // #3 Finish making Profile user blogs work
+    // #4 Make homepage work
 
     return res.json();
   } catch (error) {
